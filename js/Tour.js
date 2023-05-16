5dd4cacc8636e6b40a91ce54a81f9e8f5dc8c094
dpapanicker <70652939+dpapanicker@users.noreply.github.com>
2023-05-16 17:17:04 +0530

    Add files via upload

---
+++ js/Tour.js
@@ -0,0 +1,45 @@
+AFRAME.registerComponent("tour", {
+  init: function () {
+   
+    
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "taj-mahal",
+        title: "Taj Mahal",
+        url: "./assets/thumbnails/taj_mahal.png",
+      },
+      {
+        id: "budapest",
+        title: "Budapest",
+        url: "./assets/thumbnails/budapest.jpg",
+      },
+
+      {
+        id: "eiffel-tower",
+        title: "Eiffel Tower",
+        url: "./assets/thumbnails/eiffel_tower.jpg",
+      },
+      {
+        id: "new-york-city",
+        title: "New York City",
+        url: "./assets/thumbnails/new_york_city.png",
+      },
+    ];
+
+    
+
+
+      // Border Element
+      
+      // Thumbnail Element
+     
+      // Title Text Element
+      
+     
+    }
+  },
+  
+);
